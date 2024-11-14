2eec83d7dee3c1a15040f2a0ad751f0e4370e24d
himanshunamdeo <himanshunamdeo5@gmail.com>
2024-11-14 19:02:17 +0530

    Initial Commit: Project Creation

---
+++ MNDAssignment/MNDAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MNDAssignmentApp.swift
+//  MNDAssignment
+//
+//  Created by MeTaLlOiD on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MNDAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
